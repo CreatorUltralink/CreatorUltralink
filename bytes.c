bea17cfc4fa60759461a7bd9b1365239ae5d9130
CreatorUltralink <136919600+CreatorUltralink@users.noreply.github.com>
2023-09-26 22:40:00 -0300

    Update bytes.c

--- bytes.c
+++ bytes.c
@@ -1,60 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Definición de la estructura en C
+// Definición de una estructura para representar los datos
 struct BinaryData {
-    char* byte_0th[8][1][2][3];
-    char* byte_1st[8][4];
-    char* byte_2nd[8][5];
-    char* byte_3rd[8][5];
+    char** bits; // Matriz de punteros a caracteres para los bits
+    int num_values; // Número total de valores
 };
 
+// Función para liberar la memoria utilizada por la estructura
+void freeBinaryData(struct BinaryData* data) {
+    for (int i = 0; i < data->num_values; i++) {
+        free(data->bits[i]); // Liberar memoria de cada valor
+    }
+    free(data->bits); // Liberar memoria de la matriz
+}
+
 int main() {
-    // Declaración e inicialización de la estructura
+    // Declaración e inicialización de la estructura con los datos del JSON
     struct BinaryData data = {
-        .byte_0th = {
-            {"0"},
-            {"1"},
-            {"1", "0"},
-            {"1", "1"},
-            {"1", "0", "0"},
-            {"1", "0", "1"},
-            {"1", "1", "0"},
-            {"1", "1", "1"}
-        },
-        .byte_1st = {
-            {"1", "0", "0", "0"},
-            {"1", "0", "0", "1"},
-            {"1", "0", "1", "0"},
-            {"1", "0", "1", "1"},
-            {"1", "1", "0", "0"},
-            {"1", "1", "0", "1"},
-            {"1", "1", "1", "0"},
-            {"1", "1", "1", "1"}
-        },
-        .byte_2nd = {
-            {"1", "0", "0", "0", "0"},
-            {"1", "0", "0", "0", "1"},
-            {"1", "0", "0", "1", "0"},
-            {"1", "0", "0", "1", "1"},
-            {"1", "0", "1", "0", "0"},
-            {"1", "0", "1", "0", "1"},
-            {"1", "0", "1", "1", "0"},
-            {"1", "0", "1", "1", "1"}
-        },
-        .byte_3rd = {
-            {"1", "1", "0", "0", "0"},
-            {"1", "1", "0", "0", "1"},
-            {"1", "1", "0", "1", "0"},
-            {"1", "1", "0", "1", "1"},
-            {"1", "1", "1", "0", "0"},
-            {"1", "1", "1", "0", "1"},
-            {"1", "1", "1", "1", "0"},
-            {"1", "1", "1", "1", "1"}
-        }
+        .num_values = 8, // Número total de valores
+        .bits = (char**)malloc(sizeof(char*) * 8) // Matriz de punteros a caracteres
     };
 
-    /* Acceder a los datos */
-    printf("bytes loaded: %s\n", data.byte_0th[8][1][2][3]);
+    // Inicialización de los valores con sus bits correspondientes
+    data.bits[0] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[0], "000");
+
+    data.bits[1] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[1], "001");
+
+    data.bits[2] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[2], "010");
+
+    data.bits[3] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[3], "011");
+
+    data.bits[4] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[4], "100");
+
+    data.bits[5] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[5], "101");
+
+    data.bits[6] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[6], "110");
+
+    data.bits[7] = (char*)malloc(4 * sizeof(char));
+    strcpy(data.bits[7], "111");
+
+    // Acceso a los datos
+    for (int i = 0; i < data.num_values; i++) {
+        printf("Valor %d: %s\n", i, data.bits[i]);
+    }
+
+    // Liberar la memoria cuando hayas terminado
+    freeBinaryData(&data);
 
     return 0;
 }
